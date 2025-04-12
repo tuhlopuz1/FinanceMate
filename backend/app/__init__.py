bedef0c7f89c423f564ec45a9ea12c7f45a449dc
DukeOfTimes <107131066+DukeTime@users.noreply.github.com>
2025-04-12 23:40:45 +0300

    summary api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from backend.app.fastapi import app
 from backend.app.routes.transactions import transactions_route
 from backend.app.routes.auth import auth_route
 from backend.app.routes.ai import ai_route
+
 app.include_router(transactions_route)
 app.include_router(auth_route)
 app.include_router(ai_route)
