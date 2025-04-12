b920ea9b6e2b53bac042f86fe575aeb73ff2f76d
tuhlopuz1 <tuhlopuz1@gmail.com>
2025-04-12 12:31:14 +0300

    Мелкий дебаг фронта

--- backend/app/fastapi.py
+++ backend/app/fastapi.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi import Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 
 def lifespan(app: FastAPI):
     print("Server started")
@@ -12,3 +12,10 @@ def lifespan(app: FastAPI):
 
 # Инициализация приложения
 app = FastAPI(lifespan=lifespan, title="FinanceMate")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Или укажите конкретные домены
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
